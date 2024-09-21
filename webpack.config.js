22fd272ac3fc8b15bcf9afc16e9b4b43eba52e94
Othniel naga <81340904+othnielvtf@users.noreply.github.com>
2024-09-21 17:05:54 +0800

    use content hash for better deployment

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: 'bundle.js',
+    filename: '[contenthash].js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
